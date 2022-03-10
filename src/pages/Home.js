dcb6c0ef75c4526d9127b6e6bdd1ad0aa4622568
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-10 11:19:20 +0700

    Working Table Display User

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,51 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { loadUsersStart } from "../redux/actions";
+import {
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+  MDBBtn,
+  MDBIcon,
+  MDBTooltip,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
 
 const Home = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const {users} = useSelector(state => state.data)
 
   useEffect(() => {
-    dispatch(loadUsersStart())
-  },[])
+    dispatch(loadUsersStart());
+  }, []);
 
   return (
     <>
-      <div>
-        <h1>Home</h1>
+      <div className="container" style={{marginTop: "150px"}}>
+        <MDBTable>
+          <MDBTableHead dark>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Address</th>
+              <th scope="col">Actions</th>
+            </tr>
+            </MDBTableHead>
+            {users && users.map((item,i) => (
+              <MDBTableBody key={i}>
+                  <tr>
+                    <th scope="row">{i+1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.address}</td>
+                    <td>Edit Delete View</td>
+                  </tr>
+              </MDBTableBody>
+            ))}
+        </MDBTable>
       </div>
     </>
   );
