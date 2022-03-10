ac2483000ddb67bd7d89ca9ce11ccff6d41997ec
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-10 15:51:43 +0700

    User Info

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,11 +1,50 @@
+import { useHistory, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { MDBBtn } from "mdb-react-ui-kit";
+
 const UserInfo = () => {
-    return ( 
-        <>
-        <div>
-            <h2>User info</h2>
+  const { users } = useSelector((state) => state.data);
+  const { id } = useParams();
+  const history = useHistory();
+  const singleUser = users.find((user) => user.id === Number(id));
+
+  return (
+    <>
+      <div style={{ marginTop: "100px" }}>
+        <div
+          className="row"
+          style={{
+            margin: "auto",
+            padding: "15px",
+            maxWidth: "450px",
+            alignContent: "center",
+          }}
+        >
+          <p className="col-md-12 fs-4 fw-bold text-center">User Detail</p>
+          <hr />
+          <p className="col-md-6 fw-bold">ID:</p>
+          <p className="col-md-6">{singleUser.id}</p>
+          <p className="col-md-6 fw-bold">Name:</p>
+          <p className="col-md-6">{singleUser.name}</p>
+          <p className="col-md-6 fw-bold">Email:</p>
+          <p className="col-md-6">{singleUser.email}</p>
+          <p className="col-md-6 fw-bold">Phone:</p>
+          <p className="col-md-6">{singleUser.phone}</p>
+          <p className="col-md-6 fw-bold">Address:</p>
+          <p className="col-md-6">{singleUser.address}</p>
+          <div className="text-center mt-2">
+            <MDBBtn
+              onClick={() => history.push("/")}
+              color="danger"
+              className="text-center"
+            >
+              Go Back
+            </MDBBtn>
+          </div>
         </div>
-        </>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default UserInfo;
